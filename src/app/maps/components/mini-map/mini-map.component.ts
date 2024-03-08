2d44e790c9b4757ba70ef1ec4af4585257d8c5bb
DanteMiron <dantemiron@hotmail.com>
2024-03-08 07:47:42 -0300

    mini map

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'maplibre-gl';
+
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  @Input()
+  lngLat? : [number,number];
+
+  public map? : Map;
+
+
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap?.nativeElement ) throw "Map div nor found"
+    if ( !this.lngLat ) throw "LngLat can't be null"
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement,
+      style: 'https://api.maptiler.com/maps/openstreetmap/style.json?key=zZakkxJqH4SGaPCJ4A64', // stylesheet location
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new Marker()
+      .setLngLat( this.lngLat )
+      .addTo ( this.map )
+  }
 }
